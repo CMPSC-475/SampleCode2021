040a888fba52d97d60cff45a565506b2d26e7d7f
John Hannan <jjh9@psu.edu>
2021-09-27 14:43:26 -0400

    starter sample code for Around Town

---
+++ Around Town/Around Town/Views/MainView.swift
@@ -0,0 +1,42 @@
+//
+//  MainView.swift
+//  Mappy
+//
+//  Created by Hannan, John Joseph on 9/26/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MainView: View {
+    @EnvironmentObject var manager : MapManager
+
+    var body: some View {
+        
+        NavigationView {
+         
+            DowntownMap()
+                .toolbar {
+                    searchBarItem
+                }
+        }
+    }
+    
+    
+    // some local variables
+    var searchBarItem = ToolbarItem(placement: .navigationBarTrailing) {
+                            SearchButton()
+                        }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            MainView()
+                .environmentObject(MapManager())
+        }
+    }
+}
+
+
+
