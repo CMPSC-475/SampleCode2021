6e50ab2b5ddac2b6490da268eb26e2fbb74425e0
John Hannan <jjh9@psu.edu>
2021-09-28 14:15:56 -0400

    finished action sheet

--- Around Town/Around Town/Views/DowntownMap.swift
+++ Around Town/Around Town/Views/DowntownMap.swift
@@ -15,8 +15,14 @@ struct DowntownMap: View {
     
     var body: some View {
         Map(coordinateRegion: $manager.region, annotationItems: manager.locationModel.favorites, annotationContent: annotationFor(spot:))
-            .actionSheet(item: $spot) { Spot in
-               [ Alert.Button.cancel()]
+            .actionSheet(item: $spot) { spot in
+                ActionSheet(title: Text(spot.title ?? "No Title"),
+                            message: Text(spot.subtitle ?? "No Subtitle"),
+                            buttons: [
+                                Alert.Button.default(Text("Directions"), action: {}),
+                                Alert.Button.destructive(Text("Delete"), action: {}),
+                                Alert.Button.cancel()])
+               
             }
     }
 
